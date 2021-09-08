7a9c6a942b04f2dc0cf5fbf63a2be72c1e3f298f
annaloginova <zv1376a@gmail.com>
2021-09-08 18:35:53 +0300

    packed all code into funcs

--- trial.py
+++ trial.py
@@ -1,7 +1,5 @@
 import sqlite3 as sq
-
 import telebot
-
 from consts import token
 
 bot = telebot.TeleBot(token)
@@ -16,9 +14,6 @@ with sq.connect('db/users.db') as con:
                 metro_dep TEXT, 
                 metro_arr TEXT
               )''')
-
-m_dep = '2_20'
-m_arr = '5_6'
 
 """
 1. Получить базу станций отправления для поиска соулов
@@ -40,48 +35,58 @@ def get_num(package):
         return n
 
 
-with sq.connect('db/metro.db') as con:
-    cur = con.cursor()
+# поиск станций, которые войдут в обработку
+def find_stats(station_code):
+    with sq.connect('db/metro.db') as con:
+        cur = con.cursor()
 
-    cur.execute('''SELECT way, number FROM stations_coo WHERE code=?''', (m_dep,))
-    pack = cur.fetchall()
-    way = get_way(pack)
-    number = get_num(pack)
+        cur.execute('''SELECT way, number FROM stations_coo WHERE code=?''', (m_dep,))
+        pack = cur.fetchall()
+        way = get_way(pack)
+        number = get_num(pack)
 
-    cur.execute('''SELECT code FROM stations_coo WHERE way=? AND 
-                                                (number BETWEEN ?-2 AND ?+2)''', (way, number, number))
-    stat_pack = cur.fetchall()
+        cur.execute('''SELECT code FROM stations_coo WHERE way=? AND 
+                                                    (number BETWEEN ?-2 AND ?+2)''', (way, number, number))
+        stat_pack = cur.fetchall()
+    return stat_pack
 
-souls = []
-for i in stat_pack:
-    station_num = i[0]
+
+def find_souls(stations_pack):
+    stat_pack = stations_pack
+    souls = []
+
+    for i in stat_pack:
+        station_num = i[0]
+        with sq.connect('db/users.db') as con:
+            cur = con.cursor()
+
+            cur.execute('''SELECT user_id FROM beta_users WHERE metro_dep=?''', (station_num,))
+            soul_pack = cur.fetchmany(5)
+            for i in soul_pack:
+                soul = int(i[0])
+                souls.append(soul)
+
+    return souls
+
+
+def get_soul_info(souls_package):
+    soul_info = []
+    souls = souls_package
+
     with sq.connect('db/users.db') as con:
         cur = con.cursor()
 
-        cur.execute('''SELECT user_id FROM beta_users WHERE metro_dep=?''', (station_num,))
-        soul_pack = cur.fetchmany(1)
-        for i in soul_pack:
-            soul = int(i[0])
-            souls.append(soul)
+        for soul in souls:
+            cur.execute('''SELECT first_name, nickname, metro_dep, metro_arr FROM beta_users WHERE user_id=?''',
+                        (soul,))
+            soul_info_pack = cur.fetchall()
+            for i in soul_info_pack:
+                name = i[0]
+                nick = i[1]
+                dep = i[2]
+                arr = i[3]
 
-soul_info = []
-
-with sq.connect('db/users.db') as con:
-    cur = con.cursor()
-
-    for soul in souls:
-        cur.execute('''SELECT first_name, nickname, metro_dep, metro_arr FROM beta_users WHERE user_id=?''', (soul,))
-        soul_info_pack = cur.fetchall()
-        for i in soul_info_pack:
-            name = i[0]
-            nick = i[1]
-            dep = i[2]
-            arr = i[3]
-
-        soul_info.append(name)
-        soul_info.append(nick)
-        soul_info.append(dep)
-        soul_info.append(arr)
-
-print(souls)
-print(soul_info)
+            soul_info.append(name)
+            soul_info.append(nick)
+            soul_info.append(dep)
+            soul_info.append(arr)
