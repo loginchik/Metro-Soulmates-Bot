0c52f0b3099106a36254c0f9878fa749ffa88f8f
annaloginova <zv1376a@gmail.com>
2021-09-27 03:50:43 +0300

    improved faq

--- functions_fold/faq_funcs.py
+++ functions_fold/faq_funcs.py
@@ -1,10 +1,12 @@
 from consts import bot
 
-faq_text = 'Если в файле не найдется ответ на ваш вопрос, можете адресоватьа его @loginchik'
+faq_text = 'Все часто задаваемые вопросы собраны для удобства собраны в одном файле, и сейчас я его пришлю. ' \
+           '\n\nЕсли в файле не найдется ответ на ваш вопрос, можете адресоватьа его @loginchik'
 file_name = 'FAQ.pdf'
 
 
 def send_faq(message):
     chat_id = message.chat.id
+    bot.send_message(chat_id, faq_text)
     with open(file_name, 'rb') as file:
-        bot.send_document(chat_id, data=file, caption=faq_text)
+        bot.send_document(chat_id, data=file)
